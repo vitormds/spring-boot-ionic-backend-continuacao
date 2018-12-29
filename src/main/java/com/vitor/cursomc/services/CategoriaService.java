a70cd3e56886385e6f81354a81d088d8877170a6
Vitor Souza <vitormendes.souza@hotmail.com>
2018-12-28 22:25:50 -0300

    Criando repository para categoria

---
+++ src/main/java/com/vitor/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.vitor.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vitor.cursomc.domain.Categoria;
+import com.vitor.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+}
