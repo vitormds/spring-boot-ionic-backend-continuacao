e10ad0054c76fbd760b452628e250b9f6853eec1
Vitor Souza <vitormendes.souza@hotmail.com>
2019-01-03 23:09:12 -0300

    Estado e cidade

---
+++ src/main/java/com/vitor/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,15 @@
+package com.vitor.cursomc.repositories;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vitor.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>
+
+{
+	 
+}
