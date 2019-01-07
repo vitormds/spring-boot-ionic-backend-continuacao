c8a72195d0e71aeefbc5f3ec74d524f3fa17eb79
Vitor Souza <vitormendes.souza@hotmail.com>
2019-01-07 00:04:27 -0300

    Atualizando categoria com PUT, outros pequenos ajustes

--- src/main/java/com/vitor/cursomc/resources/ClienteResource.java
+++ src/main/java/com/vitor/cursomc/resources/ClienteResource.java
@@ -21,9 +21,9 @@ public class ClienteResource {
 		@Autowired
 		private ClienteService service;
 		@RequestMapping(value="/{id}", method=RequestMethod.GET)
-		public ResponseEntity<?> find(@PathVariable Integer id) {
+		public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 			
-			Cliente obj = service.buscar(id);
+			Cliente obj = service.find(id);
 			
 			return ResponseEntity.ok().body(obj);
 		}
