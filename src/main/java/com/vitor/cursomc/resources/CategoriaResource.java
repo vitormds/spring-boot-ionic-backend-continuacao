0b1b33e2530795067eada1be1207012a2c5b42b2
Vitor Souza <vitormendes.souza@hotmail.com>
2019-01-06 23:26:51 -0300

    Inserindo uma nova Categoria

--- src/main/java/com/vitor/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/vitor/cursomc/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.vitor.cursomc.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vitor.cursomc.domain.Categoria;
 import com.vitor.cursomc.services.CategoriaService;
@@ -26,5 +29,13 @@ public class CategoriaResource {
 			Categoria obj = service.buscar(id);
 			
 			return ResponseEntity.ok().body(obj);
+		}
+		@RequestMapping(method=RequestMethod.POST)
+		public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+			
+			obj = service.insert(obj);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+					path("/{id}").buildAndExpand(obj.getId()).toUri();
+			return ResponseEntity.created(uri).build();
 		}
 }
