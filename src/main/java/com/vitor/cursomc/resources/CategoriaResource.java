16dbe24ad5d0d90b85dfd7467b7516e9fc821961
Vitor Souza <vitormendes.souza@hotmail.com>
2019-01-10 23:43:06 -0300

    Personalizando o retorno da validacao

--- src/main/java/com/vitor/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/vitor/cursomc/resources/CategoriaResource.java
@@ -25,15 +25,14 @@ import com.vitor.cursomc.services.CategoriaService;
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	
 		@Autowired
 		private CategoriaService service;
-		
 		
 		@RequestMapping(value="/{id}", method=RequestMethod.GET)
 		public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 			
 			Categoria obj = service.find(id);
-			
 			return ResponseEntity.ok().body(obj);
 		}
 		
