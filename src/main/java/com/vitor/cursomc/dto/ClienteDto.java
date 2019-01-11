05e09cea46dbb7cb28496d6e6744fecef2bfcbd8
Vitor Souza <vitormendes.souza@hotmail.com>
2019-01-11 00:21:03 -0300

    PUT, DELETE E GET para CLIENTE

---
+++ src/main/java/com/vitor/cursomc/dto/ClienteDto.java
@@ -0,0 +1,55 @@
+package com.vitor.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.vitor.cursomc.domain.Cliente;
+
+public class ClienteDto implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
+	private String nome;
+	@NotEmpty(message="Email inválido")
+	@Email
+	private String email;
+	
+	
+	
+	public ClienteDto() {
+		super();
+	}
+	public ClienteDto(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
--- src/main/java/com/vitor/cursomc/services/ClienteService.java
+++ src/main/java/com/vitor/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.vitor.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.vitor.cursomc.domain.Cliente;
+import com.vitor.cursomc.dto.ClienteDto;
 import com.vitor.cursomc.repositories.ClienteRepository;
+import com.vitor.cursomc.services.exceptions.DataIntegrityException;
 import com.vitor.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -21,6 +28,44 @@ public class ClienteService {
 	
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: "+ Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente obj) {
 		
+	Cliente newObj = find(obj.getId());
+	updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+		
+	}
+
+	public List<Cliente> findAll() {
+		
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction) , orderBy);
+		
+		return repo.findAll(pageRequest);
+		
+	}
+	
+	public Cliente FromDTO(ClienteDto objDto) {
+		return new Cliente(objDto.getId(),objDto.getNome(),objDto.getEmail(),null,null);
 	}
 }
