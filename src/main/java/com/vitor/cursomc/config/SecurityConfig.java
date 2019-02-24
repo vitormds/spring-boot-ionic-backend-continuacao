c4e5523ef45a42c2a1fa8056279119783621828e
Vitor Souza <vitormendes.souza@hotmail.com>
2019-02-24 17:47:36 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/vitor/cursomc/config/SecurityConfig.java
+++ src/main/java/com/vitor/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.vitor.cursomc.security.JWTAuthenticationFilter;
+import com.vitor.cursomc.security.JWTAuthorizationFilter;
 import com.vitor.cursomc.security.JWTUtil;
 
 @Configuration
@@ -48,6 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers(PUBLIC_MATCHERS).permitAll()
         .anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService ));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
