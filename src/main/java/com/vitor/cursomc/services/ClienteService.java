328af5368965adf33993ef56e79df72afc090972
Vitor Souza <vitormendes.souza@hotmail.com>
2019-03-02 15:41:30 -0300

    Salvando URL da imagem em Cliente

--- src/main/java/com/vitor/cursomc/services/ClienteService.java
+++ src/main/java/com/vitor/cursomc/services/ClienteService.java
@@ -112,6 +112,15 @@ public class ClienteService {
 		return cli;
 	}
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		return s3Service.uploadFile(multipartFile);
+		UserSS user = UserService.authenticad();
+		if (user==null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		URI uri =  s3Service.uploadFile(multipartFile);
+		
+		Optional<Cliente> cli = repo.findById(user.getId());
+		cli.orElse(null).setImageUrl(uri.toString());
+		repo.save(cli.orElse(null));
+		return uri;
 	}
 }
